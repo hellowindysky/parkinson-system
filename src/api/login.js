785fd6881d46d5de6e8d34699a2088a76a5a1ea9
zhongli <neozl24@hotmail.com>
2017-07-05 15:05:08 +0800

    add api js directory

---
+++ src/api/login.js
@@ -0,0 +1,11 @@
+export function loginByEmail(email, password) {
+  const data = {
+    email,
+    password
+  };
+  return fetch({
+    url: '/login/loginbyemail',
+    method: 'post',
+    data
+  });
+};
