fa312ec93698e09c47874fbf654017219f4770d4
zhongli <neozl24@hotmail.com>
2017-07-13 11:59:34 +0800

    return lowercase name file

---
+++ src/components/particles/Particles.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div id='particles'></div>
+</template>
+
+<script>
+import 'particles.js';
+import particlesConfig from './particlesConfig.js';
+
+export default {
+  mounted() {
+    // 载入特效背景
+    window.particlesJS('particles', particlesConfig);
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#particles {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+}
+</style>
---
+++ src/components/patients/Patients.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="content-wrapper">
+    <sidebar class="sidebar"></sidebar>
+    <router-view class="list" name="list"></router-view>
+    <router-view class="content" name="content"></router-view>
+  </div>
+</template>
+
+<script>
+import sidebar from 'components/sidebar/Sidebar';
+
+export default {
+  components: {
+    sidebar
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.content-wrapper {
+  background-color: #fafafa;
+  .sidebar {
+    position: absolute;
+    width: 80px;
+    height: 100%;
+    left: 0;
+  }
+}
+</style>
