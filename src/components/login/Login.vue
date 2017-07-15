6455fbdadaf432cebae9f16b1b4ac0ddfca759ba
zhongli <neozl24@hotmail.com>
2017-07-16 00:56:52 +0800

    make login.vue more VUE-LIKE

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -5,15 +5,14 @@
       <h1 class="title">睿云</h1>
       <h3 class="subtitle">帕金森疾病病历管理系统</h3>
       <div class="tabs-wrapper">
-        <span id="accountLogin" class="tab" :class="{'current-tab':loginType===0}" @click="accountLogin">账号登录</span>
-        <span id="telLogin" class="tab" :class="{'current-tab':loginType===1}" @click="telLogin">手机登录</span>
+        <span class="tab tab-place-1" :class="{'current-tab':loginType===ACCOUNT}" @click="accountLogin">账号登录</span>
+        <span class="tab tab-place-2" :class="{'current-tab':loginType===TEL}" @click="telLogin">手机登录</span>
+        <div class="tab-bottom-bar" :class="tabPlaceClass"></div>
       </div>
-      <div id="tabBottomBar" class="tab-bottom-bar"></div>
       <div class="input-wrapper">
-        <el-input class="input" v-model="account" placeholder="请输入您的睿云账号"></el-input>
+        <el-input class="input" v-model="account" :placeholder="holderText"></el-input>
         <el-input class="input" v-model="password" placeholder="请输入6-10位数字和字母的密码"></el-input>
         <el-checkbox class="checkbox" v-model="autoLogin">自动登录</el-checkbox>
-        <span class="hint">使用账号登录</span>
       </div>
       <el-button class="button" type="primary" @click="submit">登 录</el-button>
     </div>
@@ -21,68 +20,67 @@
 </template>
 
 <script>
-import $ from 'jquery';
 import md5 from 'md5';
-import axios from 'axios';
+// import axios from 'axios';
 
 import particles from 'components/particles/Particles';
 
-const ACCOUT_LOGIN = 0;
-const TEL_LOGIN = 1;
-
-var tabBottomBar;
-var accountOffsetLeft, telOffsetLeft;
+const ACCOUNT_LOGIN = 1;
+const TEL_LOGIN = 2;
 
 export default {
   name: 'login',
   data() {
     return {
-      loginType: ACCOUT_LOGIN,
+      loginType: ACCOUNT_LOGIN,
+      ACCOUNT: ACCOUNT_LOGIN,
+      TEL: TEL_LOGIN,
       account: '',
       password: '',
       autoLogin: false
     };
   },
   computed: {
+    tabPlaceClass() {
+      return 'tab-place-' + this.loginType;
+    },
+    holderText() {
+      if (this.loginType === ACCOUNT_LOGIN) {
+        return '请输入您的睿云账号';
+      } else if (this.loginType === TEL_LOGIN) {
+        return '请输入您的手机号码';
+      }
+    },
     md5Password() {
       return md5(this.password);
     }
   },
-  mounted() {
-    /* 下划线随着点击有左右移动的动画效果 */
-    tabBottomBar = $('#tabBottomBar');
-    accountOffsetLeft = $('#accountLogin').position().left;
-    telOffsetLeft = $('#telLogin').position().left;
-    tabBottomBar.css({'transform': `translate3d(${accountOffsetLeft}px,0,0)`});
-
-    /* 下划线第一次出现的时候不希望它有动画效果，所以才用这种方式添加transition */
-    window.setTimeout(function() {
-      tabBottomBar.css({'transition': '0.3s'});
-    }, 0);
-  },
   methods: {
     accountLogin() {
-      this.loginType = ACCOUT_LOGIN;
-      tabBottomBar.css({'transform': `translate3d(${accountOffsetLeft}px,0,0)`});
+      this.loginType = ACCOUNT_LOGIN;
     },
     telLogin() {
       this.loginType = TEL_LOGIN;
-      tabBottomBar.css({'transform': `translate3d(${telOffsetLeft}px,0,0)`});
     },
     submit() {
       var self = this;
-      axios.post('http://apitest.gyenno.com/pdms/usermgr/userSignIn', {
-        'accountNumber': '15012670416',
-        'pwd': 'e10adc3949ba59abbe56e057f20f883e',
-        'userType': 1
-      })
-      .then(function(response) {
-        console.log(response);
-        self.$router.push('/home');
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+      // axios.post('http://apitest.gyenno.com/pdms/usermgr/userSignIn', {
+      //   'accountNumber': '15012670416',
+      //   'pwd': 'e10adc3949ba59abbe56e057f20f883e',
+      //   'userType': 1
+      // })
+      // .then(function(response) {
+      //   console.log(response);
+      //   self.$router.push('/patients');
+      // })
+      // .catch(function(error) {
+      //   console.log(error);
+      // });
+
+      // 暂时模拟一个登录效果
+      setTimeout(function() {
+        self.$router.push('/patients');
+      }, 1000);
     }
   },
   components: {
@@ -93,16 +91,18 @@ export default {
 
 <style lang="less" scoped>
 @import '~styles/variables.less';
+@tab-width: 60px;
+@tab-horizontal-space: 20px;
+
 .panel {
   // position: relative;
-  // margin: 100px auto;
   padding: 20px;
   width: 400px;
   height: 370px;
   position: absolute;
   left: 50%; top: 40%;
   transform: translate(-50%, -50%);
-  background-color: rgba(224,224,224,0.3);
+  background-color: rgba(224,224,224,0);
   border-radius: 30px;
   z-index: 100;
   .title {
@@ -118,14 +118,20 @@ export default {
     font-size: 18px;
   }
   .tabs-wrapper {
-    height: 20px;
-    line-height: 20px;
-    font-size: @normal-font-size;
+    position: relative;
+    margin: 0 auto;
+    padding: 0;
+    width: @tab-width * 2  + @tab-horizontal-space;
+    height: 25px;
+    line-height: 25px;
     .tab {
-      margin: auto 20px;
-      padding: 2px;
-      width: 50px;
+      position: absolute;
+      margin: 0;
+      width: @tab-width;
+      left: 0;
+      box-sizing: border-box;
       text-align: center;
+      font-size: @normal-font-size;
       color: #666;
       cursor: pointer;
       &:hover {
@@ -138,11 +144,21 @@ export default {
         color: @theme-color;
       }
     }
-  }
-  .tab-bottom-bar {
-    width: 60px;
-    height: 2px;
-    background-color: @theme-color;
+    .tab-bottom-bar {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: @tab-width;
+      height: 2px;
+      background-color: @theme-color;
+      transition: 0.2s;
+    }
+    .tab-place-1 {
+      transform: translateX(0);
+    }
+    .tab-place-2 {
+      transform: translateX(@tab-width + @tab-horizontal-space);
+    }
   }
   .input-wrapper {
     margin: 20px auto;
@@ -154,11 +170,6 @@ export default {
     .checkbox {
       margin-top: 10px;
       float: left;
-    }
-    .hint {
-      margin-top: 10px;
-      float: right;
-      font-size: @normal-font-size;
     }
   }
   .button {
