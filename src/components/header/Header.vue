a2d8650e6aaadfef32aa56f2b473a47eca610508
zhongli <neozl24@hotmail.com>
2017-08-03 12:08:44 +0800

    remove old unused css in header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -26,24 +26,6 @@ export default {
     }
   },
   methods: {
-    choosePatients() {
-      // 如果当前路径不是以“/patients”开头了，才发生跳转
-      if (!/^\/patients/.test(this.$route.path)) {
-        this.$router.push('/patients');
-      }
-    },
-    chooseAnalytics() {
-      // 如果当前路径不是以“/analytics”开头了，才发生跳转
-      if (!/^\/analytics/.test(this.$route.path)) {
-        this.$router.push('/analytics');
-      }
-    },
-    chooseConfiguration() {
-      // 如果当前路径不是以“/configuration”开头了，才发生跳转
-      if (!/^\/configuration/.test(this.$route.path)) {
-        this.$router.push('/configuration');
-      }
-    },
     togglePanel() {
       this.showPanel = !this.showPanel;
     }
@@ -90,50 +72,6 @@ export default {
     height: 360px;
     background-color: rgba(16,16,16,0.8);
     z-index: 300;
-  }
-  .tabs-wrapper {
-    position: absolute;
-    padding: 0;
-    height: @header-height;
-    left: 40%;
-    @media screen and (max-width: 900px) {
-      left: 35%;
-    }
-    @media screen and (max-width: 750px) {
-      left: 30%;
-    }
-    margin: 0;
-    font-size: 0;
-    .tab {
-      display: inline-block;
-      margin: 0;
-      padding: 0;
-      width: @tab-width;
-      font-size: @normal-font-size;
-      color: @font-color;
-      cursor: pointer;
-      &:hover {
-        color: lighten(@font-color, 20%);
-      }
-    }
-    .tab-bottom-bar {
-      position: absolute;
-      width: 80px;
-      height: 3px;
-      bottom: 0;
-      left: 10px;
-      background-color: @button-color;
-      transition: 0.15s;
-      &.tab-place-1 {
-        transform: translateX(0px);
-      }
-      &.tab-place-2 {
-        transform: translateX(@tab-width);
-      }
-      &.tab-place-3 {
-        transform: translateX(@tab-width * 2);
-      }
-    }
   }
   .operation-wrapper {
     position: absolute;
