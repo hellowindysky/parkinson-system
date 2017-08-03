80165cc3c497241b7ee39aaa45402c68a770b8d3
zhongli <neozl24@hotmail.com>
2017-08-03 15:35:49 +0800

    add overflow:hidden to entire app

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
   text-align: center;
   color: @font-color;
   user-select: none;
+  overflow: hidden;
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
 </style>
