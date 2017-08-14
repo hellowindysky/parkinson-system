70da8868cf4dbbb979044bda2079335c8e282ee2
zhongli <neozl24@hotmail.com>
2017-08-14 18:41:03 +0800

    reconstruct getInfo function

--- src/store/modules/dictionary.js
+++ src/store/modules/dictionary.js
@@ -1,6 +1,25 @@
 import {getDictionary} from 'api/user.js';
 import * as types from '../mutation_types';
 
+var getGroups = function(state, tableName) {
+  // 如果 state.all.tables 为 undefined，则返回一个空数组
+  if (!state.all.tables) {
+    return [];
+
+  } else {
+    var groups = state.all.tables.filter((table) => {
+      return table.tableName === tableName;
+    })[0].groups;
+
+    // 然后对这个数组进行加工，让它更扁平化，方便我们在组件中使用
+    var processedGroups = [];
+    for (var i = 0; i < groups.length; i++) {
+      processedGroups.push(groups[i].fields);
+    }
+    return processedGroups;
+  }
+};
+
 // initial state
 const state = {
   all: {}
@@ -8,22 +27,10 @@ const state = {
 
 const getters = {
   basicInfoDictionaryGroups: (state) => {
-    // 如果 state.all.tables 为 undefined，则返回一个包含三个空数组的数组
-    if (!state.all.tables) {
-      return [[], [], []];
-
-    } else {
-      var groups = state.all.tables.filter((table) => {
-        return table.tableName === 'tc_patient_info';
-      })[0].groups;
-
-      // 然后对这个数组进行加工，让它更扁平化，方便我们在组件中使用
-      var processedGroups = [];
-      for (var i = 0; i < groups.length; i++) {
-        processedGroups.push(groups[i].fields);
-      }
-      return processedGroups;
-    }
+    return getGroups(state, 'tc_patient_info');
+  },
+  diseaseInfoDictionaryGroups: (state) => {
+    return getGroups(state, 'tc_patient_disease_info');
   },
   typeGroup: (state) => {
     // 如果 state.all.typegroup 为 undefined，则返回一个空数组
