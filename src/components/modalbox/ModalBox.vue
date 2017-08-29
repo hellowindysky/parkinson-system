8e06420fe6036f83077a16dd457d5505d01329a6
zhongli <neozl24@hotmail.com>
2017-08-29 17:18:12 +0800

    add warning text of form-validation

--- src/components/modalbox/ModalBox.vue
+++ src/components/modalbox/ModalBox.vue
@@ -8,20 +8,21 @@
           <span class="required-mark" v-show="field.must===1">*</span>
         </span>
         <span class="field-input">
+          <span class="warning-text">{{getWarningText(field.fieldName)}}</span>
           <span v-if="getUIType(field)===1">
-            <el-input v-model="item[field.fieldName]" :class="{'warning': false}"
+            <el-input v-model="item[field.fieldName]" :class="{'warning': warningResults[field.fieldName]}"
              :placeholder="getMatchedField(field).cnFieldDesc" @change="updateWarning(field)"></el-input>
           </span>
           <span v-else-if="getUIType(field)===3">
-            <el-select v-model="item[field.fieldName]" :class="{'warning': false}"
+            <el-select v-model="item[field.fieldName]" :class="{'warning': warningResults[field.fieldName]}"
              :placeholder="getMatchedField(field).cnFieldDesc" @change="updateWarning(field)">
               <el-option v-for="type in getTypes(field)" :label="type.typeName"
-               :value="type.typeCode" :key="type.typeCode"></el-option>
+               :value="type.typeCode" :key="type.typeName"></el-option>
             </el-select>
           </span>
           <span v-else-if="getUIType(field)===6">
-            <el-date-picker v-model="item[field.fieldName]" type="date" :placeholder="getMatchedField(field).cnFieldDesc"
-             format="yyyy-MM-dd" @change="updateDate(field)"></el-date-picker>
+            <el-date-picker v-model="item[field.fieldName]" :class="{'warning': warningResults[field.fieldName]}" type="date"
+             :placeholder="getMatchedField(field).cnFieldDesc" format="yyyy-MM-dd" @change="updateDate(field)"></el-date-picker>
           </span>
         </span>
       </div>
@@ -42,7 +43,8 @@ export default {
       displayModal: false,
       modalType: '',
       title: '',
-      item: {}
+      item: {},
+      warningResults: {}
     };
   },
   computed: {
@@ -106,19 +108,33 @@ export default {
       this.title = title;
       this.item = Object.assign({}, item);
 
+      // 每次打开这个模态框，都会重新初始化 this.item
+      this.initItem();
+
       for (var i = 0; i < this.template.length; i++) {
         // console.log(this.template[i].fieldName);
       }
     },
     cancel() {
       this.displayModal = false;
+      this.clearWarning();
     },
     submit() {
+      // 首先检查是否每个字段都合格，检查一遍之后，如果 warningResults 的所有属性值都为空，就证明表单符合要求
+      for (let field of this.template) {
+        this.updateWarning(field);
+      }
+      for (let fieldName in this.warningResults) {
+        if (this.warningResults[fieldName]) {
+          return false;
+        }
+      }
+
       this.displayModal = false;
-      console.log(this.item);
+      this.clearWarning();
     },
     initItem() {
-      // 遍历当前的 template，对其中的每个 field，检查 this.item 下有没有名字对应的属性值，没有的化，就初始化为 ''
+      // 遍历当前的 template，对其中的每个 field，检查 this.item 下有没有名字对应的属性值，没有的化，就初始化为空字符串
       // 注意初始化采用 this.$set 方法，使得当前 Vue 实例对象可以跟踪该属性值的变化
       for (let field of this.template) {
         let name = field.fieldName;
@@ -156,7 +172,36 @@ export default {
       this.updateWarning(field);
     },
     updateWarning(field) {
-      return field;
+      var fieldName = field.fieldName;
+      var fieldValue = this.item[fieldName];
+      if (field.must === 1) {
+        // must 为 1 代表必填，为 2 代表选填
+        var isEmptyValue = !fieldValue && fieldValue !== 0;
+        var isEmptyArray = fieldValue instanceof Array && fieldValue.length === 0;
+        if (isEmptyValue || isEmptyArray || fieldValue.indexOf('NaN') > -1) {
+          // 下面这个方法将响应属性添加到嵌套的对象上，这样 Vue 才能实时检测和渲染
+          this.$set(this.warningResults, fieldName, '必填项');
+          return;
+        }
+      }
+
+      if (fieldValue && fieldValue.toString().indexOf(' ') > -1) {
+        this.$set(this.warningResults, fieldName, '不能包含空格');
+
+      } else {
+        // 初始化组件的时候，对应字段的警告文本为 undefined，判断之后，就为实际文本或 null
+        // null 代表该字段项的填写没有毛病
+        this.$set(this.warningResults, fieldName, null);
+      }
+    },
+    getWarningText(fieldName) {
+      var warningResult = this.warningResults[fieldName];
+      return warningResult ? warningResult : '';
+    },
+    clearWarning() {
+      for (let key in this.warningResults) {
+        this.warningResults[key] = null;
+      }
     }
   },
   mounted() {
@@ -227,6 +272,14 @@ export default {
         line-height: @field-height;
         font-size: @normal-font-size;
         color: @light-font-color;
+        .warning-text {
+          position: absolute;
+          top: 25px;
+          left: 10px;
+          height: 15px;
+          color: red;
+          font-size: @small-font-size;
+        }
         .el-input {
           .el-input__inner {
             height: 30px;
