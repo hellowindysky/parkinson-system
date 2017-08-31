37f8220efe630a4030eba149141b383b8ad17452
zhongli <neozl24@hotmail.com>
2017-08-31 11:01:49 +0800

    remove unused file

--- src/components/basicinfo/basicInfoTemplate.js
+++
@@ -1,5 +0,0 @@
-export default {
-  group1: ['id', 'patientCode', 'cardId', 'name', 'sex', 'nation', 'birthday', 'age', 'phone', 'phone2'],
-  group2: ['marryType', 'degree', 'career', 'bloodType', 'height', 'weight'],
-  group3: ['econType', 'liveType', 'homeProvince', 'homeAddress']
-};
