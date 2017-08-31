bb49306195f9350ab068fc4edcc3c3435b2438a3
zhongli <neozl24@hotmail.com>
2017-08-31 16:39:07 +0800

    add delete function to api

--- src/api/patient.js
+++ src/api/patient.js
@@ -128,6 +128,21 @@ export function modifyPatientMedHistory(medHistory) {
   return encapsulatePromise(url, request);
 };
 
+// 删除用药史
+export function deletePatientMedHistory(medHistory) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientMedHistory": medHistory
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientMedHistory';
+
+  return encapsulatePromise(url, request);
+}
+
 // 新增既往史
 export function addPatientDisease(patientDisease) {
   var request = {
@@ -154,6 +169,21 @@ export function modifyPatientDisease(patientDisease) {
     "patientDisease": patientDisease
   };
   var url = 'http://apitest.gyenno.com/pdms/modPatientDisease';
+
+  return encapsulatePromise(url, request);
+};
+
+// 删除既往史
+export function deletePatientDisease(patientDisease) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientDisease": patientDisease
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientDisease';
 
   return encapsulatePromise(url, request);
 };
@@ -188,6 +218,21 @@ export function modifyPatientFamily(patientFamily) {
   return encapsulatePromise(url, request);
 };
 
+// 删除家族史
+export function deletePatientFamily(patientFamily) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientFamily": patientFamily
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientFamily';
+
+  return encapsulatePromise(url, request);
+};
+
 // 新增锻炼史
 export function addPatientExercise(patientExercise) {
   var request = {
@@ -214,6 +259,21 @@ export function modifyPatientExercise(patientExercise) {
     "patientExercise": patientExercise
   };
   var url = 'http://apitest.gyenno.com/pdms/modPatientExercise';
+
+  return encapsulatePromise(url, request);
+};
+
+// 删除锻炼史
+export function deletePatientExercise(patientExercise) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientExercise": patientExercise
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientExercise';
 
   return encapsulatePromise(url, request);
 };
@@ -248,6 +308,21 @@ export function modifyPatientSmoke(patientSmoke) {
   return encapsulatePromise(url, request);
 };
 
+// 删除吸烟史
+export function deletePatientSmoke(patientSmoke) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientSmoke": patientSmoke
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientSmoke';
+
+  return encapsulatePromise(url, request);
+};
+
 // 新增饮酒史
 export function addPatientWine(patientWine) {
   var request = {
@@ -274,6 +349,21 @@ export function modifyPatientWine(patientWine) {
     "patientWine": patientWine
   };
   var url = 'http://apitest.gyenno.com/pdms/modPatientWine';
+
+  return encapsulatePromise(url, request);
+};
+
+// 删除饮酒史
+export function deletePatientWine(patientWine) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientWine": patientWine
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientWine';
 
   return encapsulatePromise(url, request);
 };
@@ -308,6 +398,21 @@ export function modifyPatientTea(patientTea) {
   return encapsulatePromise(url, request);
 };
 
+// 删除喝茶史
+export function deletePatientTea(patientTea) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientTea": patientTea
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientTea';
+
+  return encapsulatePromise(url, request);
+};
+
 // 新增咖啡史
 export function addPatientCoffee(patientCoffee) {
   var request = {
@@ -338,6 +443,21 @@ export function modifyPatientCoffee(patientCoffee) {
   return encapsulatePromise(url, request);
 };
 
+// 删除咖啡史
+export function deletePatientCoffee(patientCoffee) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientCoffee": patientCoffee
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientCoffee';
+
+  return encapsulatePromise(url, request);
+};
+
 // 新增毒物接触史
 export function addPatientToxicExposure(toxicExposure) {
   var request = {
@@ -364,6 +484,21 @@ export function modifyPatientToxicExposure(toxicExposure) {
     "patientCideexposed": toxicExposure
   };
   var url = 'http://apitest.gyenno.com/pdms/modPatientCideexposed';
+
+  return encapsulatePromise(url, request);
+};
+
+// 删除毒物接触史
+export function deletePatientToxicExposure(toxicExposure) {
+  var request = {
+    "userId": 93242,
+    "accountNumber": "15527231713",
+    "userType": 2,
+    "orgId": 34,
+    "orgType": 2,
+    "patientCideexposed": toxicExposure
+  };
+  var url = 'http://apitest.gyenno.com/pdms/delPatientCideexposed';
 
   return encapsulatePromise(url, request);
 };
