ce7f6c191decec305f24a6e5302f83df273eb544
zhongli <neozl24@hotmail.com>
2017-09-09 15:09:38 +0800

    change casing and return directories

---
+++ src/components/analysisconfiguration/AnalysisConfiguration.vue
@@ -0,0 +1,14 @@
+<template>
+  <feature-table-head></feature-table-head>
+</template>
+
+<script>
+import FeatureTableHead from 'components/tablehead/FeatureTableHead';
+export default {
+  components: {
+    FeatureTableHead
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
---
+++ src/components/groupconfiguration/GroupConfiguration.vue
@@ -0,0 +1,16 @@
+<template>
+  <feature-table-head></feature-table-head>
+</template>
+
+<script>
+import FeatureTableHead from 'components/tablehead/FeatureTableHead';
+export default {
+  components: {
+    FeatureTableHead
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
