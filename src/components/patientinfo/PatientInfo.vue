5f9398c8b957fa89eafebc59b6deb25e6275bfd0
zengbingbing <zengbingbing@gyenno.com>
2017-11-15 11:16:24 +0800

    comments 导出病历

--- src/components/patientinfo/PatientInfo.vue
+++ src/components/patientinfo/PatientInfo.vue
@@ -6,7 +6,7 @@
       <div class="tab right-tab" :class="{'current-tab': currentTab === 'diagnosticInfo'}"
         @click="chooseDiagnostic" v-show="existed">诊断信息</div>
       <div class="patient-id" v-show="existed">患者ID: {{patientId}}</div>
-      <div class="button" v-show="existed && listType==='myPatients'">导出病历</div>
+      <!-- <div class="button" v-show="existed && listType==='myPatients'">导出病历</div> -->
       <div class="tab-bottom-bar" :class="currentTabBottomBar" v-show="this.existed"></div>
       <div class="title" v-show="!existed">新增患者</div>
     </div>
