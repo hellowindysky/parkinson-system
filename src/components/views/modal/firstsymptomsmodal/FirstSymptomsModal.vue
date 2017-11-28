897e102e4c2e0440c2898c2a3ee1818b65aefad5
zhangquan <zhangquan@gyenno.com>
2017-11-28 16:43:43 +0800

    modal

---
+++ src/components/views/modal/firstsymptomsmodal/FirstSymptomsModal.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="less">
+@import "~styles/variables.less";
+</style>
+
