964413652aab394d7d2464d0b613a1f9eeeb6b1b
zhongli <neozl24@hotmail.com>
2017-12-02 17:59:32 +0800

    add new component -- subjectPanel

---
+++ src/components/public/subjectpanel/SubjectPanel.vue
@@ -0,0 +1,197 @@
+<template lang="html">
+  <div class="subject-panel">
+    <div class="iconfont icon-close" @click="closePanel"></div>
+    <p class="title">添加课题（带颜色的为已加入的课题，点击标签即可加入或移出）</p>
+    <div class="subject-wrapper" ref="scrollArea">
+      <div class="subject-item" v-for="(subject, index) in allSubjects"
+        :class="{'selected': subjectSelectedList[index]}" @click="toggleSelected(index)">
+        {{subject.subjectName}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Ps from 'perfect-scrollbar';
+import { addPatientToSubject, removePatientFromSubject } from 'api/patient.js';
+
+var lockList = [];  // 这个数组用来标记正在发生状态改变的分组
+
+export default {
+  props: {
+    display: {
+      type: Boolean,
+      default: false
+    },
+    patientId: {
+      type: Number,
+      default: ''
+    },
+    belongSubjects: {
+      type: Array,
+      default: () => {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      allSubjects: [],
+      subjectSelectedList: []
+    };
+  },
+  computed: {},
+  methods: {
+    closePanel() {
+      this.$emit(this.HIDE_SUBJECT_PANEL);
+    },
+    updateScrollbar() {
+      this.$nextTick(() => {
+        Ps.destroy(this.$refs.scrollArea);
+        Ps.initialize(this.$refs.scrollArea, {
+          wheelSpeed: 1,
+          minScrollbarLength: 40
+        });
+      });
+    },
+    toggleSelected(index) {
+      if (lockList.indexOf(index) >= 0) {
+        return;   // 请求在发送过程中时，再次点击没有效果
+      }
+      lockList.push(index);
+      var value = !this.subjectSelectedList[index];
+      this.$set(this.subjectSelectedList, index, value);
+      var subjectId = this.allSubjects[index].leafId;
+      var patientId = Number(this.patientId);
+      var patientSubject = {
+        taskInfoId: subjectId,
+        patientId: patientId
+      };
+      if (value) {
+        addPatientToSubject([patientSubject]).then(() => {
+          let listIndex = lockList.indexOf(index);
+          this.$emit(this.UPDATE_PATIENT_SUBJECT_INFO);
+          lockList.splice(listIndex, 1);
+        }, (error) => {
+          console.log(error);
+        });
+      } else {
+        removePatientFromSubject([patientSubject]).then(() => {
+          let listIndex = lockList.indexOf(index);
+          this.$emit(this.UPDATE_PATIENT_SUBJECT_INFO);
+          lockList.splice(listIndex, 1);
+        }, (error) => {
+          console.log(error);
+        });
+      }
+    }
+  },
+  watch: {
+    display: function(val) {
+      if (val === true) {
+        var subjects = sessionStorage.getItem('subjects');
+        console.log(subjects);
+        for (let subject of subjects) {
+          var subjectName = subject.taskName;
+          var subjectId = subject.leafId;
+
+        }
+        // this.allSubjects = data;
+        // let length = this.allSubjects.length;
+        // this.subjectSelectedList = [];
+        // for (var i = 0; i < length; i++) {
+        //   var value = false;
+        //   for (let belongSubject of this.belongSubjects) {
+        //     if (this.allSubjects[i].subjectId === belongSubject.subjectId) {
+        //       value = true;
+        //     }
+        //   }
+        //   this.$set(this.subjectSelectedList, i, value);
+        // }
+        // this.updateScrollbar();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "~styles/variables.less";
+
+.subject-panel {
+  display: block;
+  padding: 10px 0;
+  width: @subject-panel-width;
+  height: @subject-panel-height;
+  box-sizing: border-box;
+  background-color: @theme-color;
+  .iconfont {
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    font-size: 22px;
+    color: #fff;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.6;
+    }
+    &:active {
+      opacity: 0.8;
+    }
+  }
+  .title {
+    margin: 10px 25px;
+    font-size: @normal-font-size;
+    color: #fff;
+    text-align: left;
+  }
+  .subject-wrapper {
+    position: relative;
+    height: 340px;
+    padding: 0 10px;
+    text-align: left;
+    overflow: hidden;
+    .subject-item {
+      display: inline-block;
+      margin: 5px 3%;
+      padding: 0 5px;
+      width: 27.33%;
+      height: 30px;
+      line-height: 30px;
+      box-sizing: border-box;
+      background-color: #fff;
+      color: @light-font-color;
+      text-align: center;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      transition: color 0.1s;
+      cursor: pointer;
+      &.selected {
+        background-color: @button-color;
+        color: #fff;
+      }
+      &:hover {
+        opacity: 0.9;
+      }
+    }
+    .ps__scrollbar-y-rail {
+      position: absolute;
+      width: 10px;
+      right: 0;
+      box-sizing: border-box;
+      opacity: 0.6;
+      transition: opacity 0.3s;
+      .ps__scrollbar-y {
+        position: relative;
+        background-color: @inverse-font-color;
+      }
+    }
+    &:hover {
+      .ps__scrollbar-y-rail {
+        opacity: 0.8;
+      }
+    }
+  }
+}
+</style>
