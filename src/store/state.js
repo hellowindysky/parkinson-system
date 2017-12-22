47be0e1ccd1ae0bea475753a286d8b0a998857a6
zhongli <neozl24@hotmail.com>
2017-12-22 14:36:50 +0800

    use vuex to manage showSensitiveInfo

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,8 @@
 const state = {
   lastOperationTime: (new Date()).getTime(),
   subjectId: parseInt(sessionStorage.getItem('subjectId'), 10),
-  hasRightToDisplaySensitiveInfo: false    // 这个变量代表是否已经通过“全部显示”的短信验证，不代表当前系统的实际脱敏与否
+  hasRightToDisplaySensitiveInfo: false,  // 这个变量代表是否已经通过“全部显示”的短信验证，不代表当前系统的实际脱敏与否
+  showSensitiveInfo: false  // 表示是否显示脱敏字段，默认为“否”，即处于脱敏状态
 };
 
 export default state;
