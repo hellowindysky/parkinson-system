8d88b1037141ad05de00f038e2feedf4ffec81dd
zhongli <neozl24@hotmail.com>
2018-01-03 13:40:26 +0800

    add router guard

--- src/components/views/patient/experimentinfo/ExperimentInfo.vue
+++ src/components/views/patient/experimentinfo/ExperimentInfo.vue
@@ -6,8 +6,18 @@
 
 <script>
 export default {
+  beforeRouteEnter(to, from, next) {
+    var subjectId = sessionStorage.getItem('subjectId');
+    if (subjectId > 0) {
+      next();
+    } else {
+      next(from.path);
+    }
+  }
 };
 </script>
 
 <style lang="less">
+@import "~styles/variables.less";
+
 </style>
