7955e579b3f09cc58787dacef68929fd3cccd9f8
zhongli <neozl24@hotmail.com>
2018-01-03 16:40:32 +0800

    add .

--- src/components/views/patient/experimentinfo/ExperimentInfo.vue
+++ src/components/views/patient/experimentinfo/ExperimentInfo.vue
@@ -1,6 +1,30 @@
 <template lang="html">
   <div class="experiment-info">
-
+    <div class="top-bar">
+      <span class="title">实验流程</span>
+    </div>
+    <div class="content">
+      <table class="process-table">
+        <tr class="row title-row">
+          <td class="col col-number">序号</td>
+          <td class="col col-progress">节点</td>
+          <td class="col col-executor">执行人</td>
+          <td class="col col-status">状态</td>
+          <td class="col col-start-time">开始时间</td>
+          <td class="col col-end-time">结束时间</td>
+          <td class="col col-remarks">处理意见</td>
+        </tr>
+        <tr class="row">
+          <td class="col col-number">1</td>
+          <td class="col col-progress">筛选入组</td>
+          <td class="col col-executor">增增增</td>
+          <td class="col col-status">完成</td>
+          <td class="col col-start-time">2017-12-12 19:00</td>
+          <td class="col col-end-time">2017-1-7 18:00</td>
+          <td class="col col-remarks">该患者符合课题要求，申请入组</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -20,4 +44,66 @@ export default {
 <style lang="less">
 @import "~styles/variables.less";
 
+.experiment-info {
+  box-sizing: content-box;
+  background-color: @screen-color;
+  .top-bar {
+    width: 100%;
+    height: 40px;
+    margin-bottom: 5px;
+    padding-left: 20px;
+    box-sizing: border-box;
+    text-align: left;
+    background-color: @background-color;
+    .title {
+      display: inline-block;
+      height: 40px;
+      line-height: 40px;
+      font-size: @large-font-size;
+      font-weight: bold;
+    }
+  }
+  .content {
+    width: 100%;
+    background-color: @background-color;
+    .process-table {
+      width: 100%;
+      border-spacing: 0;
+      table-layout: fixed;
+      // border: 1px solid @light-gray-color;
+      border-collapse: collapse;
+      .row {
+        width: 100%;
+        height: 35px;
+        &.title-row {
+          background-color: @light-font-color;
+          color: #fff;
+        }
+        .col {
+          &.col-number {
+            width: 10%;
+          }
+          &.col-progress {
+            width: 15%;
+          }
+          &.col-executor {
+            width: 15%;
+          }
+          &.col-status {
+            width: 15%;
+          }
+          &.col-start-time {
+            width: 20%;
+          }
+          &.col-end-time {
+            width: 20%;
+          }
+          &.col-remarks {
+            width: 30%;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
