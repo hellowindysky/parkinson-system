1f0ce2c5b610bcf3ca7ae47398bb1c3f97b3c477
zhongli <neozl24@hotmail.com>
2018-01-16 16:42:19 +0800

    input style update for type === number

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,10 @@ export default {
   user-select: none;
   overflow: hidden;
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none !important;
+    margin: 0;
+  }
 }
 </style>
