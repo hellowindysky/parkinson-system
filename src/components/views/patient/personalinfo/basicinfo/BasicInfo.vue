bd626fd3ee22c5be066ee12ad33c84ef1328f33f
zhongli <neozl24@hotmail.com>
2018-01-25 10:45:23 +0800

    add missing code

--- src/components/views/patient/personalinfo/basicinfo/BasicInfo.vue
+++ src/components/views/patient/personalinfo/basicinfo/BasicInfo.vue
@@ -238,13 +238,7 @@ export default {
 
         }, (error) => {
           console.log(error);
-          if (error.code === 23) {
-            this.$message({
-              message: '该用户已经存在',
-              type: 'error',
-              duration: 2000
-            });
-          } else if (error.code === 31) {
+          if (error.code === 31) {
             this.$message({
               message: '医院患者编码重复',
               type: 'error',
@@ -259,6 +253,19 @@ export default {
           } else if (error.code === 2007) {
             this.$message({
               message: '住院病例号重复',
+              type: 'error',
+              duration: 2000
+            });
+          } else if (error.code === 2008) {
+            this.$message({
+              message: '身份证号重复',
+              type: 'error',
+              duration: 2000
+            });
+          } else if (error.code === 2011) {
+            // 姓名相同，而且联系电话存在重复
+            this.$message({
+              message: '该患者已存在，请检查姓名和联系电话',
               type: 'error',
               duration: 2000
             });
