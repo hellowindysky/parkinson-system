5bebea42eed521c8013fd0731a12fb4858d715dd
zhangyuandian <1002872775@qq.com>
2018-05-15 11:05:46 +0800

    添加动态验证接口

--- src/api/login.js
+++ src/api/login.js
@@ -1,10 +1,11 @@
 /* eslint-disable quotes */
 import { encapsulatePromise } from 'api/common.js';
 
-export function getLoginInfo(accountNumber, pwd) {
+export function getLoginInfo(accountNumber, pwd, identifyingCode) {
   var request = {
     'accountNumber': accountNumber,
-    'pwd': pwd
+    'pwd': pwd,
+    'identifyingCode': identifyingCode
   };
   var url = '/usermgr/userSignIn';
   return encapsulatePromise(url, request);
